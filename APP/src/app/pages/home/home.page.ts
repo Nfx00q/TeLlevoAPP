8c6155fcede1c474078b7ddbf9253527af35b1fb
Nfx00q <babgutier@gmail.com>
2024-10-10 22:19:30 -0300

    Diseño boton home cambiado

--- APP/src/app/pages/home/home.page.ts
+++ APP/src/app/pages/home/home.page.ts
@@ -14,13 +14,6 @@ export class HomePage implements OnInit {
 
   usuarios: Usuario[] = [];
   presentingElement = null;
-  
-  markers = [
-    { lat: -33.5791787663939, lng: -70.58202650110043 },
-    { lat: -33.55333862632548, lng: -70.5867276319482 },
-    { lat: -33.50828814033571, lng: -70.61039431815159 },
-    { lat: -33.66833314724194, lng: -70.58517694562185 },
-  ];
 
   public nombreUsuario?: string;
   public apellidoUsuario?: string;
@@ -64,17 +57,11 @@ export class HomePage implements OnInit {
 
       L.marker([-33.598460591168845, -70.57876561786227], { icon: customIcon }).addTo(map);
 
-      this.markers.forEach(marker => {
-        L.marker([marker.lat, marker.lng])
-          .addTo(map)
-          .openPopup();
-      });
-
       map.setView([-33.59805940505581, -70.57816364636162], 15);
     }, 1000);
   }
 
   goToConfig(){
-    this.router.navigate(['/config-page'])
+    this.router.navigate(['/config-page']);
   }
 }
